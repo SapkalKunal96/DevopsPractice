c5da17f664b781017f66155c13a77ccdb44168ce
= <=>
2025-01-27 09:34:46 +0530

    Final pipeline with When condition

--- 01.S3bucketStaticWebHosting/terraform/s3.tf
+++ 01.S3bucketStaticWebHosting/terraform/s3.tf
@@ -32,46 +32,46 @@ module "s3_bucket" {
   tags = var.bucket_tags
 }
 
-resource "aws_s3_object" "index_html" {
-  bucket       = module.s3_bucket.s3_bucket_id
-  key          = "index.html"
-  source       = "../website/index.html"
-  content_type = "text/html"
-}
+# resource "aws_s3_object" "index_html" {
+#   bucket       = module.s3_bucket.s3_bucket_id
+#   key          = "index.html"
+#   source       = "../website/index.html"
+#   content_type = "text/html"
+# }
 
-resource "aws_s3_object" "css" {
-  for_each     = fileset("../website/css", "*")
-  bucket       = module.s3_bucket.s3_bucket_id
-  key          = "css/${each.value}"
-  source       = "../website/css/${each.value}"
-  content_type = "text/css"
-}
+# resource "aws_s3_object" "css" {
+#   for_each     = fileset("../website/css", "*")
+#   bucket       = module.s3_bucket.s3_bucket_id
+#   key          = "css/${each.value}"
+#   source       = "../website/css/${each.value}"
+#   content_type = "text/css"
+# }
 
-resource "aws_s3_object" "img" {
-  for_each     = fileset("../website/img", "*")
-  bucket       = module.s3_bucket.s3_bucket_id
-  key          = "img/${each.value}"
-  source       = "../website/img/${each.value}"
-  content_type = "image/jpg"
-}
-
-
-resource "aws_s3_object" "js" {
-  for_each     = fileset("../website/js", "*")
-  bucket       = module.s3_bucket.s3_bucket_id
-  key          = "js/${each.value}"
-  source       = "../website/js/${each.value}"
-  content_type = "application/javascript"
-}
+# resource "aws_s3_object" "img" {
+#   for_each     = fileset("../website/img", "*")
+#   bucket       = module.s3_bucket.s3_bucket_id
+#   key          = "img/${each.value}"
+#   source       = "../website/img/${each.value}"
+#   content_type = "image/jpg"
+# }
 
 
-resource "aws_s3_object" "video" {
-  for_each     = fileset("../website/video", "*")
-  bucket       = module.s3_bucket.s3_bucket_id
-  key          = "video/${each.value}"
-  source       = "../website/video/${each.value}"
-  content_type = "video/mp4"
-}
+# resource "aws_s3_object" "js" {
+#   for_each     = fileset("../website/js", "*")
+#   bucket       = module.s3_bucket.s3_bucket_id
+#   key          = "js/${each.value}"
+#   source       = "../website/js/${each.value}"
+#   content_type = "application/javascript"
+# }
+
+
+# resource "aws_s3_object" "video" {
+#   for_each     = fileset("../website/video", "*")
+#   bucket       = module.s3_bucket.s3_bucket_id
+#   key          = "video/${each.value}"
+#   source       = "../website/video/${each.value}"
+#   content_type = "video/mp4"
+# }
 
 
 
