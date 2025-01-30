84080a37aaecd418e0c03b892ca1d7ce70248ee5
= <=>
2025-01-30 22:56:26 +0530

    Update dthe vpcPerring file startup

---
+++ 02.aws/vpcEndpoint/main.tf
@@ -0,0 +1,22 @@
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.7.1"
+}
+
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "single-instance"
+
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
